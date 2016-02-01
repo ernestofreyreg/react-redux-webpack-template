32b75d9474f4349a7c4d2448a53ec720373cbd78
Ernesto Freyre Gonzalez <ernestofreyreg@gmail.com>
2016-01-31 19:52:35 -0800

    Some fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
+import './App.scss';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <div className="container">
-          <div className="page-header">
-            <h1>React-Redux-Webpack Template</h1>
-          </div>
-          <p className="lead">A simple starting template to create simple Single Page Apps.</p>
-          <p>Have fun coding... <i className="fa fa-thumbs-o-up"/></p>
+      <div className="App">
+        <div className="page-header">
+          <h1>React-Redux-Webpack Template</h1>
         </div>
-
-        <footer className="footer">
-          <div className="container">
-            <p className="text-muted"><b>react-redux-webpack-template</b> by Ernesto Freyre</p>
-          </div>
-        </footer>
+        <p className="lead">A simple starting template to create simple Single Page Apps.</p>
+        <p>Have fun coding... <i className="fa fa-thumbs-o-up"/></p>
       </div>
     );
   }
